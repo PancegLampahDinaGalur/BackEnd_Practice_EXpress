e3e6aa3578f9046cb98aa90a149ba6fc6c14ec43
unknown <localuser@N5204787.toyota.co.id>
2024-10-08 16:30:52 +0700

    Aaut && Oauth

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const PORT = 3000;
-//const routes = require("./src/routes");
+const routes = require("./src/routes");
 
 const app = express();
 const server = http.createServer(app);
@@ -15,8 +15,8 @@ app.use(express.json());
 
 require("./src/routes")(app);
 
-// app.use(express.json()); // Sebagai middleware untuk mengubah req.body menjadi JSON
-// app.use(routes);
+app.use(express.json()); // Sebagai middleware untuk mengubah req.body menjadi JSON
+app.use(routes);
 
 
 app.use((req, res) => {
