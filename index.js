28dc9b72688d6bde49b46efff0b7f828c64f0bac
unknown <localuser@N5204787.toyota.co.id>
2024-10-23 09:29:51 +0700

    update swagger v2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const routes = require("./src/routes");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./src/swagger.json");
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // untuk swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // menambahkan swagger UI
 
 //registrasi error handler secara global
 require("./src/helpers/errors");
